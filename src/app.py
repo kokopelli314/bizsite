ddd0f93a4148e57a598f40980cdc2ec738c004d4
Nathan Clonts <nacclonts@gmail.com>
2019-04-21 20:33:35 -0600

    directory structure re-org for flask

--- src/app.py
+++ src/app.py
@@ -1,8 +1,10 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+app = Flask(__name__, static_folder='static', static_url_path='/static')
 
 @app.route('/')
-def hello():
-	return 'What up!'
+def homepage():
+	return render_template('index.html')
 
-
+@app.route('/contact')
+def contact():
+	return ''
