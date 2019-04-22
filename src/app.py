91730aa6b5ed76847c10aa98d1c16fe21c203c7f
Nathan Clonts <nacclonts@gmail.com>
2019-04-21 20:44:20 -0600

    use base template

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-app = Flask(__name__, static_folder='static', static_url_path='/static')
+app = Flask(__name__)
 
 @app.route('/')
 def homepage():
@@ -7,4 +7,4 @@ def homepage():
 
 @app.route('/contact')
 def contact():
-	return ''
+	return render_template('contact.html')
